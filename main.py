e336edcf7e03418600e8efe6154ca3d929431876
Boy-Zee <boyzee1266@gmail.com>
2025-05-12 15:05:44 +0100

    removed the run on main

--- main.py
+++ main.py
@@ -4,7 +4,3 @@ from app import app
 
 # Configure logging for easier debugging
 logging.basicConfig(level=logging.DEBUG)
-
-if __name__ == "__main__":
-    # Start the Flask app, binding to all network interfaces on port 5000
-    app.run(host="0.0.0.0", port=5000, debug=True)
